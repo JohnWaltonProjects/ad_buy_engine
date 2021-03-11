9fc359e2f84663290ad7fc6f21aaf54f7cd78fc9
John W <johnwalton@protonmail.com>
2021-03-10 17:11:21 -0800

    Init Commit

---
+++ src/constant/server_info.rs
@@ -0,0 +1,11 @@
+pub const ROOT_DOMAIN: &'static str = "https://adbuyengine.com";
+pub const HOST_DOMAIN: &'static str = "adbuyengine.com";
+pub const CLICK_DOMAIN: &'static str = "click.adbuyengine.com";
+pub const CAMPAIGN_SERVER_URL: &'static str = "http://127.0.0.1:8088";
+pub const CLICK_SERVER_URL: &'static str = "http://127.0.0.1:33441";
+pub const CLICK_SERVER_IP_PORT_TERSE: &'static str = "127.0.0.1:33441";
+pub const MAIN_SERVER_IP: &'static str = "127.0.0.1";
+pub const CAMPAIGN_SERVER_IP_PORT_TERSE: &'static str = "0.0.0.0:8088";
+pub const CLICK_SERVER_IP: &'static str = "127.0.0.1";
+pub const DNS_LINODE_API_TOKEN: &'static str =
+    "44f46522d3554f7e1b92495b0209db33f31608d2a3ee5ad8c06e7c008b547489";
